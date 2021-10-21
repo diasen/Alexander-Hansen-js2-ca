48d26604a55a21aa737ad851233a2deb070db050
Alexander Hansen <Alexander-hansen@hotmail.com>
2021-10-21 21:01:30 +0200

    lvl1 done

--- js/favourites.js
+++ js/favourites.js
@@ -7,6 +7,12 @@ let favourites = getFromLocalStorage('favourites');
 console.log(favourites);
 
 let favouriteCards = document.querySelector('.favouriteCards');
+const clearLocalStorage = document.querySelector('.clearBtn');
+
+if (favourites.length === 0) {
+  document.querySelector('.favouriteCards').innerHTML =
+    '<h5>You have not selected any favourites</h5>';
+}
 
 favourites.forEach(({ id, title, summary, author }) => {
   favouriteCards.innerHTML += `
@@ -17,9 +23,15 @@ favourites.forEach(({ id, title, summary, author }) => {
                     <p class="card-text">${summary}</p>
                     <div>
                         <a href="#" class="card-link">Read more</a>
-                        <i class="far fa-heart" data-id="${id}" data-author="${author}" data-title="${title}" data-summary="${summary}"></i>
+                        <i class="fas fa-heart" data-id="${id}" data-author="${author}" data-title="${title}" data-summary="${summary}"></i>
                     </div>  
                 </div>
             </div>
 `;
 });
+
+clearLocalStorage.onclick = function () {
+  localStorage.clear();
+  document.querySelector('.favouriteCards').innerHTML =
+    '<h5>All favourites removed</h5>';
+};
